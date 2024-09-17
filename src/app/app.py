115d0e34c0e2636cb2bf42a37aaaedc6d5b8366c
Bruno Augusto <120321738+imbrunoagc@users.noreply.github.com>
2024-09-16 21:21:12 -0300

    fix: first version app

--- src/app/app.py
+++ src/app/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from sqlalchemy import create_engine
-
+from datetime import datetime, time
 import altair as alt
 
 dir_current = os.path.dirname(os.path.abspath(__file__))
@@ -14,13 +14,11 @@ from generate_tables_fake import create_tables_fake
 
 
 from configs.settings import setup_page
-from pages import home, analytics
-from components.sidebar import sidebar
+#from components.sidebar import sidebar
 from components.footer import footer
 
 # Configs of page
 setup_page()
-
 
 DONWLOAD_FAKER = False
 
@@ -50,24 +48,28 @@ if st.checkbox('Show raw data'):
     st.write(df_viagem.shape)
     st.write(df_viagem)
 
-
 #######################
-# Columns header
+# Columns header - Metrics
 col1, col2, col3 = st.columns(3)
 
-col1\
-    .metric("Number of races",
-            df_viagem['id_viagem'].nunique())
+col1.metric(
+    "Number of races",
+    f"{df_viagem['id_viagem'].nunique():,.0f}".replace(",", ".")
+)
 
 total_km = df_viagem['distancia_km'].sum()
 col2\
-    .metric("Total km driven",f'{total_km:.2f}')
+    .metric("Total km driven",f'{total_km:,.2f}'.replace(",", "X").replace(".", ",").replace("X", "."))
 
 total_price = df_viagem['preco_viagem'].sum()
 col3\
-    .metric("Total price of races", f'R$ {total_price:.2f}')
+    .metric("Total price of races", f'R$ {total_price:,.2f}'.replace(",", "X").replace(".", ",").replace("X", "."))
+
+#######################
+# little transformations - DateTime
 
 df_viagem['data_viagem'] = pd.to_datetime(df_viagem['data_viagem'])
+df_viagem = df_viagem.sort_values(['data_viagem'], ascending=True)
 
 # Criar colunas separadas para ano, mês e hora
 df_viagem['date'] = df_viagem['data_viagem'].dt.date
@@ -76,7 +78,8 @@ df_viagem['mes'] = df_viagem['data_viagem'].dt.month
 df_viagem['hora'] = df_viagem['data_viagem'].dt.hour
 
 #######################
-col1_filter_date, col2_filter_month, col3_filter_hour = st.columns(3)
+# Columns filter
+col1_filter_date, col2_filter_hour = st.columns(2)
 
 list_dates = df_viagem['date'].sort_values(ascending=True).unique()
 
@@ -87,31 +90,44 @@ chosen_date = col1_filter_date.date_input(
     max_value=list_dates[-1],
     format="MM.DD.YYYY"
 )
-#st.write(f"Chosen date: {chosen_date}")
 
-choose_month = col2_filter_month.selectbox(
-    "Choose your month",
-    options=df_viagem['mes'].unique()
+# Slider para escolher intervalo de tempo
+appointment = col2_filter_hour.select_slider(
+    "Choose Your Date and Hour:", 
+    options=df_viagem['data_viagem'],
+    value=(df_viagem['data_viagem'].min(), df_viagem['data_viagem'].max()),
+    format_func=lambda x: x.strftime('%Y-%m-%d %H:%M')
 )
 
-choose_hour = col3_filter_hour.selectbox(
-    "Choose your hour",
-    options=df_viagem['hora'].sort_values(ascending=True).unique()
-)
-
+start_datetime = appointment[0]
+end_datetime = appointment[1]
 
 
 #######################
-# text content
+# Visual Graphics
+
+with col1_filter_date:
+    agg_hour = df_viagem[
+        (df_viagem['date'] == chosen_date)
+        ].groupby('hora').agg(
+        count_races=('id_viagem', 'count')
+    ).reset_index()
+
+    st.line_chart(data=agg_hour, x='hora', y=['count_races'])
+
+
+with col2_filter_hour:
+    agg_month = df_viagem[
+        (df_viagem['data_viagem'] >= start_datetime) & 
+        (df_viagem['data_viagem'] <= end_datetime)
+        ].groupby('mes').agg(
+        count_races=('id_viagem', 'nunique')
+    ).reset_index()
+
+    st.bar_chart(data=agg_month, x='mes', y='count_races', x_label='Horarios de corrida', y_label='Quantidade de corridas', stack='layered')
 
 
 
-#######################
-# text content
-
-
-#######################
-# text content
 
 
 
